9a9832d3a42250578d9597212e221c1cbc39d2ff
Michael Ingeman-Nielsen <michael.ingeman-nielsen@eficode.com>
2020-05-25 00:29:50 +0200

    enable cleaning up when removing containers

--- helper_functions.ps1
+++ helper_functions.ps1
@@ -7,12 +7,16 @@ function exit_on_error {
 }
 
 function exit_if_running {
+    param (
+        [bool]$remove=$false
+    )
     Write-Host "Checking if Bitbucket container is running..."
     docker ps -qf name="^bitbucket$" *>&1
     $wasrunning=$?
     if ($?) {
         Write-Host "Stopping Bitbucket during backup..."
-        $result=docker-compose -f bitbucket-compose.yml -f postgres-compose.yml -f traefik-compose.yml stop *>&1
+        $command = If ($remove) {"down"} Else {"stop"}
+        $result=docker-compose -f bitbucket-compose.yml -f postgres-compose.yml -f traefik-compose.yml -f backup-compose.yml $command *>&1
         exit_on_error $result
         Write-Host "Bitbucket will be restarted after the backup"
     } else {
